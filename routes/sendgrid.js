134413f5ea8e1e6b72d6def4831136903bafccdb
panforrest <guoqianp@gmail.com>
2018-01-04 16:18:04 -0500

    sendgrid api test

--- routes/sendgrid.js
+++ routes/sendgrid.js
@@ -1,65 +1,44 @@
 var express = require('express');
 var router = express.Router();
+var sendgrid = require('sendgrid')
+var helper = require('sendgrid').mail;
+var from_email = new helper.Email('gzpgg3x@yahoo.com');
+var to_email = new helper.Email('guoqianp@gmail.com');
+var subject = 'Hello World from the SendGrid Node.js Library!';
+var content = new helper.Content('text/plain', 'Hello, Email!');
+var mail = new helper.Mail(from_email, subject, to_email, content);
 
 router.get('/', function(req, res, next) {
-	res.json({
-		confirmation: 'success',
-		message: 'sendgrid works'
-	})
+	var sg = require('sendgrid')(process.env.SENDGRID_API_KEY);
+	var request = sg.emptyRequest({
+	  method: 'POST',
+	  path: '/v3/mail/send',
+	  body: mail.toJSON(),
+	});
+
+	sg.API(request, function(error, response) {
+	  console.log(response.statusCode);
+	  console.log(response.body);
+	  console.log(response.headers);
+
+	  if (error) {
+	  	res.json({
+	  		confirmation: 'fail',
+	  		message: errorr
+	  	})
+
+	  	return
+	  }
+
+	  res.json({
+	  	confirmation: 'success',
+	  	message: response.body
+	  })
+
+	  return
+
+	});
 
 })
 
 module.exports = router;
-// var express = require('express');
-// var router = express.Router();
-// var sendgrid = require('sendgrid')
-
-// var helper = require('sendgrid').mail;
-// var from_email = new helper.Email('guoqianp@gmail.com');
-// var to_email = new helper.Email('gzpgg3x@gmail.com');
-// var subject = 'Hello World from the SendGrid Node.js Library!';
-// var content = new helper.Content('text/plain', 'Hello, Email!');
-// var mail = new helper.Mail(from_email, subject, to_email, content);
-
-// /* GET users listing. */
-// router.get('/', function(req, res, next) {
-// 	var sg = require('sendgrid')(process.env.SENDGRID_API_KEY);
-// 	var request = sg.emptyRequest({
-// 	  method: 'POST',
-// 	  path: '/v3/mail/send',
-// 	  body: mail.toJSON(),
-// 	});
-
-// 	sg.API(request, function(error, response) {
-// 	  console.log(response.statusCode);
-// 	  console.log(response.body);
-// 	  console.log(response.headers);
-
-// 		if (error){
-// 			res.json({
-// 				confirmation: 'fail',
-// 				message: error
-// 			})
-
-// 			return
-// 		}
-
-// 		res.json({
-// 			confirmation: 'success',
-// 			response: response.body    //but wny?
-// 		})
-
-// 	    return
-
-
-
-// 	});
-
-
-// });
-
-// module.exports = router;
-
-
-
-// // router.get()
