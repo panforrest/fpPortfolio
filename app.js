2de313081336e8565c59f28fe72efb40664a0e07
panforrest <guoqianp@gmail.com>
2018-01-04 16:05:15 -0500

    Oct25 2:02:09 connected to sendgrid within heroku; sendgrid route works

--- app.js
+++ app.js
@@ -4,9 +4,11 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+require('dotenv').config()
 
 var routes = require('./routes/index');
 var users = require('./routes/users');
+var sendgrid = require('./routes/sendgrid');
 
 var app = express();
 
@@ -26,6 +28,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
 app.use('/users', users);
+app.use('/sendgrid', sendgrid);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
